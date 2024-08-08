f334054cb3921c6503e1720c8fe82a3356621690
jbowen999 <julian.e.bowen@outlook.com>
2024-08-08 15:29:18 -0700

    Class 8/8

---
+++ BinarySearchTree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treez
+{
+    public class BinarySearchTree
+    {
+        private Node<int>? Root;
+        private int Count = 0;
+
+        public bool DoesValueExist(int valuesToFind)
+        {
+            Node<int> currentNode = Root;
+
+            while (currentNode != null)
+            {
+                if(currentNode.Data == valuesToFind) 
+                    return true;
+                else if(currentNode.Data == valuesToFind)
+                {
+                    currentNode = currentNode.Left;
+                } else
+                {
+                    currentNode = currentNode.Right;
+                }
+            }
+            return false;
+
+        }
+        public void Insert(int data)
+        {
+            Node<int> newNode = new Node<int>(data);
+            InsertHelper(newNode, Root);
+            
+
+        }
+
+        public void InsertHelper(Node<int> newNode, Node<int>? currentNode)
+        {
+            if (Root == null)
+            {
+                Root = newNode;
+                Count++;
+                return;
+            }
+
+            //go left
+            if (newNode.Data <= currentNode.Data)
+            {
+                if (currentNode.Left == null)
+                {
+                    currentNode.Left = newNode;
+                }
+                else
+                {
+                    InsertHelper(newNode, currentNode.Left);
+                }
+
+            }
+            else // go right
+            {
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.Insert(25);
+            tree.Insert(22);
+            tree.Insert(13);
+
         }
+
     }
 }
